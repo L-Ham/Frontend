6ce0e74ad7ef3aff37fc6ee1e7e8c63fe0bd3b33
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-03-12 16:50:19 +0200

    Setup ESlint & JSDoc
    - Used Google JS Style for ESLint
    - Setup example JSDoc string
    To follow: https://google.github.io/styleguide/jsguide.html

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import TestHeader from './TestHeader';
+import {ChakraProvider} from '@chakra-ui/react';
+
+/**
+ * Main application component
+ * @component
+ * @param {Object} props - The component props
+ * @example
+ * // Render the application
+ * <App />
+ * @return {JSX.Element} The main application component
+ */
 function App() {
-  return (
-    <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider>
+            <div className="App">
+                <TestHeader />
+            </div>
+        </ChakraProvider>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, test} from '@testing-library/react';
+// Import the 'expect' function from the 'jest-dom' package
+import {expect} from '@testing-library/jest-dom';
 import App from './App';
+import React from 'react';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
