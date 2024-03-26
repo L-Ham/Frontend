f4e71cd06edfbafbab5e5ca83b2ede2c3ccb20ef
Sherif-Hatem1 <125387417+Sherif-Hatem1@users.noreply.github.com>
2024-03-27 00:53:09 +0200

    login components

---
+++ src/__tests__/guestpagecomponents/logincomponents/heading.js
@@ -0,0 +1,23 @@
+/*eslint-disable*/
+import React from "react";
+/**
+ * 
+ * @returns {JSX.Element} Log in
+ 
+ */
+function Heading() {
+  return (
+    <h1 className="ibm-plex-sans-serif"
+      style={{
+        textAlign: "left",
+        fontSize: 18,
+        fontWeight: 500,
+        color: "#1a1a1b",
+        fontFamily: "IBMPlexSans, sans-serif",
+      }}
+    >
+      Log in
+    </h1>
+  );
+}
+export { Heading };
