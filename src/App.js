ef3d7188b444ef4aa6aa87d0a467de341b8458de
Abdulrahman Elbedewy <96942793+AbdulrahmanEl-Bedewy@users.noreply.github.com>
2024-03-27 22:31:58 +0200

    Added a generic layout component
    Added a generic component that wraps children components and adds the app and side bars

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {SideBar} from './layouts/SideBar/sidebar';
 import {AppBar} from './layouts/Header/appbar';
+import {LayoutWithNavigation} from './generic components/layoutwithnavigation';
 import './App.css';
 
 /**
@@ -15,27 +16,15 @@ import './App.css';
  */
 function App() {
     return (
-        <div>
-            <div className='flex justify-evenly pt-[56px]'>
-                <div id='header-container' className='fixed inset-x-0 top-0 z-[4] nd:visible'>
-                    <AppBar />
-                </div>
-                <div className='m:grid-cols-[272px_1fr] grid grid-cols-1'>
-                    <div id='sidebar-container' className='isolate order-first hidden nd:block'>
-                        <SideBar visible={true} />
-                    </div>
-                    <div id='main-container' className='block h-[calc(100vh-56px)] w-full flex-col'>
-                        <Router>
-                            <Routes>
-                                <Route path='/' element={<div>Home</div>} />
-                                <Route path='/popular' element={<div>Popular</div>} />
-                                <Route path='/all' element={<div>All</div>} />
-                            </Routes>
-                        </Router>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <LayoutWithNavigation>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<div>Home</div>} />
+                    <Route path='/popular' element={<div>Popular</div>} />
+                    <Route path='/all' element={<div>All</div>} />
+                </Routes>
+            </Router>
+        </LayoutWithNavigation>
     );
 }
 
