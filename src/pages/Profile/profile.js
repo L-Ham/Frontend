95ca4e8bf5674c922f016492652f65a9a12fb549
mohzakaria <95968167+mohzakaria@users.noreply.github.com>
2024-04-06 17:54:42 +0200

    User Profile

---
+++ src/pages/Profile/profile.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Profile() {
+    return (
+        <div className="order-2 block mx-auto box-border flex w-full flex-col m:px-4">
+            
+        </div>
+    );
+}
