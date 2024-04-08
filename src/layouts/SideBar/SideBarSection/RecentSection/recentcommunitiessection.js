6ad134ad6484c041b05a126bdc3342e693efd159
Abdulrahman Elbedewy <96942793+AbdulrahmanEl-Bedewy@users.noreply.github.com>
2024-04-08 12:20:40 +0200

    Recent community section hooks refactor

--- src/layouts/SideBar/SideBarSection/RecentSection/recentcommunitiessection.js
+++ src/layouts/SideBar/SideBarSection/RecentSection/recentcommunitiessection.js
@@ -1,7 +1,8 @@
-import {React, useState} from 'react';
+import React from 'react';
 import {RecentCommunityItem} from './recentcommunityitem';
 import {SectionHeader} from '../sectionheader';
-import {sectionClasses as styles} from '../sidebarsection.styles';
+// import {sectionClasses as styles} from '../sidebarsection.styles';
+import {useRecentCommunitiesSection} from './recentcommunities.hooks';
 
 /**
  * The sidebar recent communities section component
@@ -12,21 +13,12 @@ import {sectionClasses as styles} from '../sidebarsection.styles';
  * @return {JSX.Element} The sidebar recent communities section component
  */
 function RecentCommunitiesSection() {
-    const [isOpen, setIsOpen] = useState(true);
-    const rootStyles = isOpen ? `${styles.root} ${styles.open}` : `${styles.root} ${styles.close}`;
-    const recentCommunities = [
-        {
-            // eslint-disable-next-line max-len
-            imgURL: 'https://styles.redditmedia.com/t5_323oy/styles/communityIcon_wqodb68q5gca1.jpg?format=pjpg&s=41993445a49aa828a9f9996e00867bb94fb03269',
-            label: 'r/CasualConversation',
-            href: 'https://www.reddit.com/r/CasualConversation/',
-        },
-        {
-            imgURL: 'https://styles.redditmedia.com/t5_2qh1i/styles/communityIcon_p6kb2m6b185b1.png',
-            label: 'r/AskReddit',
-            href: 'https://www.reddit.com/r/AskReddit/',
-        },
-    ];
+    const {
+        isOpen,
+        setIsOpen,
+        rootStyles,
+        recentCommunities,
+    } = useRecentCommunitiesSection();
     return (
         <>
             <SectionHeader sectionName='RECENT' isOpen={isOpen} setIsOpen={setIsOpen} />
