8dfeeae98ecf8021a65f13f9d173cc08361ecf84
gaser sami <gaser.elmasry02@eng-st.cu.edu.eg>
2024-04-08 19:46:16 +0200

    [Refactor] Update UserCard component test

--- src/__tests__/subreddit/usercard.test.js
+++ src/__tests__/subreddit/usercard.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import {describe, it, expect} from '@jest/globals';
-import {UserCard} from '../../pages/subreddit/usercard';
+import {UserCard} from '../../pages/subreddit/SubredditSidebar/Widgets/Moderators/Usercard/usercard';
 
 describe('UserCard', () => {
     // Renders user card with all required props
@@ -10,33 +10,11 @@ describe('UserCard', () => {
         const name = 'testUser';
         const displayName = 'Test User';
         const pictureSrc = 'test.jpg';
-        const isLink = true;
 
-        render(<UserCard name={name} displayName={displayName} pictureSrc={pictureSrc} isLink={isLink} />);
+        render(<UserCard name={name} displayName={displayName} pictureSrc={pictureSrc} />);
 
-        expect(new URL(screen.getByAltText('picture').src).pathname).toEqual(`/${pictureSrc}`);
+        expect(new URL(screen.getByAltText(`u/${name} avatar`).src).pathname).toEqual(`/${pictureSrc}`);
         expect(screen.getByText(`u/${name}`)).toBeInTheDocument();
         expect(screen.getByText(displayName)).toBeInTheDocument();
-        expect(screen.getByText(`u/${name}`).closest('a')).
-            toHaveAttribute('href', `https://www.reddit.com/user/${name}/`);
-        expect(screen.getByText(`u/${name}`).closest('a')).toHaveAttribute('target', '_blank');
-        expect(screen.getByText(`u/${name}`).closest('a')).toHaveAttribute('rel', 'noreferrer');
-    });
-
-    // Renders user card with empty name
-    it('should render user card with empty name', () => {
-        const name = '';
-        const displayName = 'Test User';
-        const pictureSrc = 'test.jpg';
-        const isLink = true;
-
-        render(<UserCard name={name} displayName={displayName} pictureSrc={pictureSrc} isLink={isLink} />);
-
-        expect(new URL(screen.getByAltText('picture').src).pathname).toEqual(`/${pictureSrc}`);
-        expect(screen.getByText(`u/${name}`)).toBeInTheDocument();
-        expect(screen.getByText(displayName)).toBeInTheDocument();
-        expect(screen.getByText(`u/${name}`).closest('a')).toHaveAttribute('href', '#');
-        expect(screen.getByText(`u/${name}`).closest('a')).toHaveAttribute('target', '_blank');
-        expect(screen.getByText(`u/${name}`).closest('a')).toHaveAttribute('rel', 'noreferrer');
     });
 });
