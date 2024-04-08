4b73654f22efbedb410b2d2546188ed028ea55ec
Abdulrahman Elbedewy <96942793+AbdulrahmanEl-Bedewy@users.noreply.github.com>
2024-04-08 12:19:56 +0200

    Resource section hooks refactor

---
+++ src/layouts/SideBar/SideBarSection/ResourcesSection/resources.hooks.js
@@ -0,0 +1,83 @@
+import {useState} from 'react';
+import {sectionClasses} from '../sidebarsection.styles';
+
+// resources section hooks
+export const useResourcesSection = () => {
+    const [isOpen, setIsOpen] = useState(true);
+    const rootStyles = isOpen ? `${sectionClasses.root} ${sectionClasses.open}` :
+        `${sectionClasses.root} ${sectionClasses.close}`;
+    const resources = [
+        [
+            {
+                icon: 'admin',
+                label: 'About Reddit',
+                href: 'https://www.redditinc.com/',
+            },
+            {
+                icon: 'topic-activism',
+                label: 'Advertise',
+                href: 'https://www.redditinc.com/advertising',
+            },
+            {
+                icon: 'help',
+                label: 'Help ',
+                href: 'https://www.reddit.com/r/help/',
+            },
+            {
+                icon: 'topic-reading',
+                label: 'Blog ',
+                href: 'https://www.redditblog.com/',
+            },
+            {
+                icon: 'topic-career',
+                label: 'Careers',
+                href: 'https://www.redditinc.com/careers',
+            },
+            {
+                icon: 'author',
+                label: 'Press',
+                href: 'https://www.redditinc.com/press',
+            },
+        ],
+        [
+            {
+                icon: 'community',
+                label: 'Communities',
+                href: 'https://www.reddit.com/best/communities/1/',
+            },
+            {
+                icon: 'topic-history',
+                label: 'Best of reddit',
+                href: 'https://www.reddit.com/posts/2023/',
+            },
+            {
+                icon: 'topic',
+                label: 'Topics',
+                href: 'https://www.reddit.com/topics/a-1/',
+            },
+        ],
+        [
+            {
+                icon: 'topic-ethics',
+                label: 'Content Policy',
+                href: 'https://www.redditinc.com/policies/content-policy',
+            },
+            {
+                icon: 'topic-law',
+                label: 'Privacy Policy',
+                href: 'https://www.redditinc.com/policies/privacy-policy',
+            },
+            {
+                icon: 'rules',
+                label: 'User Agreement',
+                href: 'https://www.redditinc.com/policies/user-agreement',
+            },
+        ],
+    ];
+    return {
+        isOpen,
+        setIsOpen,
+        rootStyles,
+        resources,
+    };
+};
