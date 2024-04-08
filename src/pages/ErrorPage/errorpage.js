2f8d1185c6ee82a3a42e5697498f8ffa457bb4a6
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-04-08 17:04:24 +0200

    [Unit Tests] Error Page

--- src/pages/ErrorPage/errorpage.js
+++ src/pages/ErrorPage/errorpage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {getIconComponent} from '../../generic components/iconsmap';
+import {NavLink} from 'react-router-dom';
 /**
  * The error page component.
  * @return {React.Component} The error page component.
@@ -7,12 +8,18 @@ import {getIconComponent} from '../../generic components/iconsmap';
 export function ErrorPage() {
     const DizzySnooIconOutline = getIconComponent('dizzy-snoo', false);
     return (
-        <div className='flex h-screen flex-col items-center justify-center' style={{fontFamily: 'Arial, sans-serif'}}>
-            <DizzySnooIconOutline />
-            <h3 className='my-4 text-[1.17em] font-bold'>
+        <div
+            className='flex h-screen flex-col items-center justify-center'
+            style={{fontFamily: 'Arial, sans-serif'}}
+            data-testid='error-page'
+        >
+            <DizzySnooIconOutline data-testid='dizzy-snoo-icon' />
+            <h3 className='my-4 text-[1.17em] font-bold' data-testid='error-message'>
                 Something went wrong
             </h3>
-            <a href='/' className='cursor-pointer underline'>Go home</a>
+            <NavLink to='/' className='cursor-pointer text-blue-500 underline' data-testid='go-home-link'>
+                Go home
+            </NavLink>
         </div>
     );
 }
