d22d90cc6315c2b262d1289eb1ca1c766499da46
gaser sami <gaser.elmasry02@eng-st.cu.edu.eg>
2024-04-08 19:08:00 +0200

    [Refactor] UserCard component

---
+++ src/pages/subreddit/SubredditSidebar/Widgets/Moderators/Usercard/usercard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {userCardClasses as classes} from './usercard.styles';
+
+/**
+ * Renders the user flair.
+ * @param {string} name - The username of the user.
+ * @param {string} displayName - The display name of the user.
+ * @param {string} pictureSrc - The source of the profile picture.
+ * @return {JSX.Element} The rendered component.
+ */
+export function UserCard({name, displayName='', pictureSrc}) {
+    return (
+        <li className={classes.container}>
+            <div className={classes.innerContainer}>
+                <span className={classes.imageContainer}>
+                    <span className={classes.imageWrapper}>
+                        <span className={classes.imageInnerWrapper}>
+                            <img
+                                src={pictureSrc}
+                                alt={`u/${name} avatar`}
+                                className={classes.image}
+                            />
+                        </span>
+                    </span>
+                </span>
+                <span className={classes.textContainer}>
+                    <span className={classes.username}>
+                        <a
+                            className={classes.usernameLink}
+                            target="_blank"
+                            onClick={() => window.open(`/user/${name}`, '_blank')}
+                        >
+                            {`u/${name}`}
+                        </a>
+                    </span>
+                    <span className={classes.displayName}>
+                        {displayName}
+                    </span>
+                </span>
+                <span className={classes.extraContainer}>
+                    <span className={classes.extraContent} />
+                </span>
+            </div>
+        </li>
+    );
+}
+
+UserCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    displayName: PropTypes.string,
+    pictureSrc: PropTypes.string.isRequired,
+};
---
+++ src/pages/subreddit/SubredditSidebar/Widgets/Moderators/Usercard/usercard.styles.js
@@ -0,0 +1,14 @@
+export const userCardClasses = {
+    container: 'group relative mt-0 list-none text-left',
+    innerContainer: 'relative flex gap-2 px-4 py-1 text-secondary',
+    imageContainer: 'flex min-w-0 shrink items-center gap-2',
+    imageWrapper: 'flex size-8 shrink-0 items-center justify-center',
+    imageInnerWrapper: 'relative inline-block size-8 rounded-full overflow-hidden',
+    image: 'max-w-full',
+    textContainer: 'flex min-w-0 shrink flex-col justify-center py-[var(--rem6)]',
+    username: 'text-sm',
+    usernameLink: 'text-neutral-content hover:underline cursor-pointer',
+    displayName: 'text-xs text-secondary-weak',
+    extraContainer: 'flex shrink-0 items-center',
+    extraContent: 'flex h-6 items-center justify-center',
+};
