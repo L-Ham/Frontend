ec44f07f0bacf0c9c98c1dd4637b1a6db190ddd5
gaser sami <gaser.elmasry02@eng-st.cu.edu.eg>
2024-04-08 19:01:53 +0200

    [Refactor] Add JoinButton component to SubredditBanner

---
+++ src/pages/subreddit/SubredditBanner/HeaderButtons/JoinButton/joinbutton.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {useJoinButton} from './joinbutton.hooks';
+
+/**
+ * JoinButton component
+ * @component
+ * @param {Object} props - The component props
+ * @param {string} props.subscribeLabel - The subscribe label
+ * @param {string} props.unSubscribeLabel - The unsubscribe label
+ * @param {function} props.handleJoinClick - The handle join click function
+ * @param {boolean} props.isSubscribed - The is subscribed status
+ * @return {JSX.Element} The JoinButton component
+ */
+export function JoinButton({
+    subscribeLabel,
+    unSubscribeLabel,
+    handleJoinClick,
+    isSubscribed,
+}) {
+    const {
+        handleClick,
+        disabled,
+        buttonClasses,
+        buttonLabel,
+    } = useJoinButton(handleJoinClick, isSubscribed, subscribeLabel, unSubscribeLabel);
+
+
+    return (
+        <button
+            className={buttonClasses}
+            onClick={handleClick}
+            disabled={disabled}
+        >
+            {buttonLabel}
+        </button>
+    );
+}
+
+JoinButton.propTypes = {
+    subscribeLabel: PropTypes.string,
+    unSubscribeLabel: PropTypes.string,
+    handleJoinClick: PropTypes.func,
+    isSubscribed: PropTypes.bool,
+};
+
