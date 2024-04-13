9ed072ef4dadc69b8e49d674d72a178cefcb111e
Basel Hossam <14712020102067@stud.cu.edu.eg>
2024-04-13 13:38:47 +0200

    adjusted routes for functionalities

--- src/requests/routes.js
+++ src/requests/routes.js
@@ -1,3 +1,5 @@
+
+
 // Define your API endpoints as key-value pairs for easy reference
 const API_ROUTES = {
     /* Example
@@ -5,6 +7,18 @@ const API_ROUTES = {
     CAST_VOTE: '/api/vote',
     */
     login: '/auth/login',
+    accountSettings: '/user/accountSettings',
+    profileSettings: '/user/profileSettings',
+    safetySettings: '/user/safetyAndPrivacySettings',
+    feedSettings: '/user/feedSettings',
+    notificationSettings: '/user/notificationsSettings',
+    banner: '/user/banner',
+    avatar: '/user/avatarImage',
+    blockUser: '/user/blockUser',
+    unblockUser: '/user/unblockUser',
+    muteCommunity: '/user/muteCommunity',
+    emailSettings: '/user/emailSettings',
+    chatSettings: '/user/messagingSettings',
     // Add more routes as needed
 };
 
