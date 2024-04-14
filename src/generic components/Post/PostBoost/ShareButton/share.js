59dccd518b1b39f8230132ab7a723202ef6fc982
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-04-14 14:07:14 +0200

    [Integration] Vote + Share

--- src/generic components/Post/PostBoost/ShareButton/share.js
+++ src/generic components/Post/PostBoost/ShareButton/share.js
@@ -2,33 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {shareClasses, shareStyles} from './share.styles.js';
 import {useShare} from './share.hooks.js';
+import {Dropdown} from './Dropdown/dropdown.js';
+
 /**
  * Share Button component
  * @param {string} postId
+ * @param {string} url
  * @return {React.Component}
  */
 export function Share({
     postId,
+    url,
 }) {
-    const {ShareIcon} = useShare();
+    const {
+        ShareIcon,
+        isOpen,
+        setIsOpen,
+    } = useShare();
     return (
-        <button
-            className={shareClasses.root}
-            style={shareStyles.root}
-            onClick={(e) => e.stopPropagation()}
-            type="button"
-            data-testid={`share-button-${postId}`}
-        >
-            <div className={shareClasses.wrapper} data-testid={`share-wrapper-${postId}`}>
-                <div className={shareClasses.icon} data-testid={`share-icon-${postId}`}>
-                    <ShareIcon />
+        <>
+            <button
+                className={shareClasses.root}
+                style={shareStyles.root}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setIsOpen(!isOpen);
+                }}
+                type="button"
+                data-testid={`share-button-${postId}`}
+            >
+                <div className={shareClasses.wrapper} data-testid={`share-wrapper-${postId}`}>
+                    <div className={shareClasses.icon} data-testid={`share-icon-${postId}`}>
+                        <ShareIcon />
+                    </div>
+                    Share
                 </div>
-                Share
-            </div>
-        </button>
+            </button>
+            <Dropdown
+                postId={postId}
+                url={url}
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+            />
+        </>
     );
 }
 
 Share.propTypes = {
     postId: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
 };
