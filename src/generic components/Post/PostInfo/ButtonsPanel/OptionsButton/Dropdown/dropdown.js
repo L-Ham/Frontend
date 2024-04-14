587bd0dd57db0e2b0533d20bb32898fbfd314d9c
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-04-15 01:16:12 +0200

    [Post Share] Fixed Classs Default

--- src/generic components/Post/PostInfo/ButtonsPanel/OptionsButton/Dropdown/dropdown.js
+++ src/generic components/Post/PostInfo/ButtonsPanel/OptionsButton/Dropdown/dropdown.js
@@ -37,7 +37,7 @@ export function Dropdown({
                 setIsOpen(false);
             }},
     ];
-    const classes = `absolute top-4 ${isOpen ? 'hidden': 'flex'}  flex-col overflow-hidden 
+    const classes = `absolute top-4 ${isOpen ? 'flex': 'hidden'}  flex-col overflow-hidden 
     ${isSubscriber ? '-right-8 min-w-[120px]' : '-right-24 min-w-[240px]'}
      rounded-lg bg-[var(--color-neutral-background-strong)] z-[2] max-h-[25.5rem] overflow-y-auto
      shadow-[rgba(0,0,0,0.1)_0px_4px_8px_0px,rgba(0,0,0,0.25)_0px_6px_12px_0px]`;
