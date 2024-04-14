8851927fc66c30bc6067bdfbd0bcad045dc5179e
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-04-14 14:06:27 +0200

    [Integration] Data + BackButton

--- src/generic components/Post/data.js
+++ src/generic components/Post/data.js
@@ -1,59 +1,118 @@
 export const DATA = {
-    t3_1bmnuhw: {
-        name: 't3_1bmnuhw',
-        subreddit_id: 't5_33vsv',
-        subreddit: 'csMajors',
-        subreddit_name_prefixed: 'r/csMajors',
-        author_fullname: 't2_bll4twvt',
-        author: 'Fit-Courage3123',
-        title: 'Struggling to get a job has impacted all my relationships',
-        postType: 'image',
-        permalink: '/r/csMajors/comments/1bugpq6/struggling_to_get_a_job_has_impacted_all_my/',
-        created: 1712109452.0,
-        selftext_html: `&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;p&gt;I am a 21 yo girl who` +
-        ` is studying CS in EU and have been struggling to find an internship for more than a yea` +
-        `r now. And that has affected me and the people around me so much. &lt;/p&gt;\n\n&lt;p&gt` +
-        `;I am at a very bad place right now. I am only getting up to study for my exams and go t` +
-        `o classes because, I know I would be majorly fucked up if I didn’t and I do have dreams ` +
-        `and hope for my master. &lt;/p&gt;\n\n&lt;p&gt;I do apply to a few jobs everyday, email ` +
-        `people, but have gotten 2 interviews so far and have been rejected. I don’t know what to` +
-        ` do, how to improve myself. I am so lost. Everyone at Uni is moving forward, I don’t kno` +
-        `w how, they’re the people I hang out with at uni, they just don’t tell me things. &lt;/p` +
-        `&gt;\n\n&lt;p&gt;I have a good GPA, it’s almost straight As. That doesn’t help, there’s ` +
-        `always better candidates and profiles. I have been ignored, ghosted, rejected for a year` +
-        `. Still continuing to do so. I have applied to tech roles and research roles yet nothing` +
-        `. &lt;/p&gt;\n\n&lt;p&gt;I stopped my contact with my family back home, they don’t under` +
-        `stand. Every time we talk, they tell me that it will be fine, or I should have stayed ba` +
-        `ck home, or joke about it. It is so draining. I am not okay. &lt;/p&gt;\n\n&lt;p&gt;My o` +
-        `lder sister who was also a CS major, and has a job and lives in a more stable EU country` +
-        ` has totally turned her back on me. It has been 4 months since we talked. And whenever I` +
-        ` text, she’s super formal, almost like a stranger. &lt;/p&gt;\n\n&lt;p&gt;I don’t have a` +
-        `ny really friends, people that I know outside unis are privileged people (not in cs). I ` +
-        `can’t keep hearing about their love affairs when I am mentally and financially strugglin` +
-        `g and don’t know who to talk with or go to. &lt;/p&gt;\n\n&lt;p&gt;I was such a great an` +
-        `d ambitious kid. I don’t know why I turned into such a waste. I feel pathetic. I am so b` +
-        `roken. I failed myself. I don’t know where to go. My time management sucks. Honestly I d` +
-        `on’t know what to do.&lt;/p&gt;\n\n&lt;p&gt;I grew up poor, I must escape the loop. I am` +
-        ` scared. &lt;/p&gt;\n\n&lt;p&gt;I have even gone to a few places near my accommodation t` +
-        `o ask for a normal job. They’re not hiring. &lt;/p&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;`,
+    't3_1bmnuhw': {
+        poll: {
+            options: [],
+            votingLength: 0,
+            voters: [],
+        },
+        _id: 't3_1bmnuhw',
+        subredditName: 'csMajors',
+        user: 't2_bll4twvt',
+        title: 'I tried to shave my head, but now I look like a worm...',
+        text: 'I tried',
+        approved: false,
+        approvedBy: '65f5f5c26c3fec25017c306d',
+        disapproved: false,
+        disapprovedBy: '65f5f5c26c3fec25017c306d',
+        type: 'image',
+        url: '/r/csMajors/comments/1bugpq6/struggling_to_get_a_job_has_impacted_all_my/',
+        createdAt: '2024-04-13T18:24:40.420Z',
+        isNSFW: true,
+        isSpoiler: false,
+        isLocked: false,
+        isOc: true,
+        isUpvoted: false,
+        isDownvoted: false,
+        imageUrls: [`https://preview.redd.it/previous-post-deleted-illegal-jaw-fixed-after-someone-sen` +
+        `t-v0-dqtd3sm5iktc1.jpeg?width=750&format=pjpg&auto=webp&s=d2f399ca7b8109d3bbff4b445ed093a32ae0c432`],
         commentCount: 96,
-        score: 598,
+        upvotes: 569,
+        downvotes: 0,
     },
-    t2_bll4twvt: {
-        id: 'bll4twvt',
-        name: 'DiscordBotStudio',
-        display_name_prefixed: 'u/DiscordBotStudio',
-        snoovatar_img: 'https://i.redd.it/snoovatar/avatars/6121c00e-73cd-4315-bd28-cb7d73d53e38.png',
-        snoovatar_size: [380, 600],
-        public_description: 'Creator of DiscordBotStudio.org and Loopliz.com',
-        comment_karma: 21,
+    't3_1bvwbgd': {
+        _id: 't3_1bvwbgd',
+        subredditName: 'csMajors',
+        user: 't2_bll4twvt',
+        title: 'Bombed my first interview :(',
+        type: 'text',
+        url: '/r/csMajors/comments/1bugpq6/struggling_to_get_a_job_has_impacted_all_my/',
+        createdAt: '2024-04-13T18:24:40.420Z',
+        isNSFW: false,
+        isSpoiler: false,
+        isLocked: false,
+        isOc: true,
+        isUpvoted: false,
+        isDownvoted: false,
+        text: `&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;p&gt;I&amp;#39;ve applied to ~400 internships` +
+        ` total across the past two years, and this is was my first interview ever and I think I failed it lol. For ` +
+        `what they were describing as the workload for this internship I felt that I fit it perfectly, but my brain ` +
+        `froze and I failed to make myself stand out by not going more in-depth to my projects and not asking better` +
+        ` questions. RIP me (probably).&lt;/p&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;`,
+        commentCount: 1244,
+        upvotes: 23423,
+        downvotes: 43,
+    },
+    't3_1c2k4vg': {
+        _id: 't3_1c2k4vg',
+        subredditName: 'MBA',
+        user: 't2_y6ddqo2ek',
+        title: 'What. Is. Wrong. With. Me.',
+        type: 'text',
+        url: '/r/MBA/comments/1c2k4vg/what_is_wrong_with_me/',
+        createdAt: '2024-04-13T18:24:40.420Z',
+        isNSFW: false,
+        isSpoiler: true,
+        isUpvoted: false,
+        isDownvoted: false,
+        isOc: true,
+        isLocked: false,
+        text: `&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;p&gt;Hey everyone! &lt;/p&gt;\n\n&lt;p&gt;I’` +
+        `m a first-year student at a T10 school. I’ve been recruiting for Tech internships (mostly PM roles, but ot` +
+        `her product roles as well) since August 2023. I’ve had ONE interview so far. \nSo I’d like you to share yo` +
+        `ur experience/ tell me what the hell is wrong with my profile. &lt;/p&gt;\n\n&lt;p&gt;Here are some things` +
+        ` I already know: \n1. Yes the economy sucks. But honestly, one interview?? Really?? I know people in my sc` +
+        `hool who are at least getting interviewed. \n2. I’m an international student. So yeah, there’s a cut off t` +
+        `here as well. &lt;/p&gt;\n\n&lt;p&gt;My profile:\n1. I worked in the network and virtualization space as a` +
+        ` program manager (focused on release management), for the past 6 years. \n2. I’ve launched over 30 product` +
+        ` releases during this time.\n3. No I don’t know coding, and no my system design knowledge is garbage. \n4.` +
+        ` I’ve done a tonne of cross functional leadership and engaged with stakeholders at all levels in the organ` +
+        `ization (depending on the project I was handling)\n5. I’ve had some ERP consulting experience before this.` +
+        ` &lt;/p&gt;\n\n&lt;p&gt;What have I been doing?\n1. I’m networking with my alumni and generally on LinkedI` +
+        `n as well.\n2. I’m applying mindfully, I make sure that I run a thorough check on my resume and make some ` +
+        `changes that highlight the skills the company is looking for. &lt;/p&gt;\n\n&lt;p&gt;Please be honest with` +
+        ` me. I’m sick of hearing the economy is bad and you’re an international student. Or oh nothing is wrong wi` +
+        `th you, it all just sucks right now. &lt;/p&gt;\n\n&lt;p&gt;I’ve talked to career service folks here and t` +
+        `hey’ve given me the same story, like just keep doing what you’re doing. &lt;/p&gt;\n\n&lt;p&gt;There must ` +
+        `be something more I can do. It’s mid-April already! What am I missing? &lt;/p&gt;\n&lt;/div&gt;&lt;!-- SC_` +
+        `ON --&gt;`,
+        commentCount: 41,
+        upvotes: 2,
+        downvotes: 2,
+    },
+    't2_bll4twvt': {
+        userId: 'bll4twvt',
+        displayName: 'DiscordBotStudio',
+        avatar: 'https://i.redd.it/snoovatar/avatars/6121c00e-73cd-4315-bd28-cb7d73d53e38.png',
+        description: 'Creator of DiscordBotStudio.org and Loopliz.com',
+        commentKarma: 21,
         created: 1632450514.0,
-        total_karma: 6,
-        link_karma: 171,
-        is_friend: false,
+        postKarma: 171,
+        isFriend: false,
+        isBlocked: false,
     },
-    t5_33vsv: {
-        id: 't5_33vsv',
+    't2_y6ddqo2ek': {
+        userId: 'y6ddqo2ek',
+        displayName: 'Altruistic-Exam-1854',
+        avatar: null,
+        description: 'I am a bot. Beep boop.',
+        commentKarma: 20,
+        created: 1632450514.0,
+        postKarma: 16,
+        isFriend: false,
+        isBlocked: false,
+    },
+    'csMajors': {
+        subredditId: 't5_33vsv',
         display_name_prefixed: 'r/csMajors',
         banner_background_image: `https://styles.redditmedia.com/t5_2sokd/styles/bannerBackgroundImag` +
         `e_at8s941lsun31.png`,
@@ -65,6 +124,208 @@ export const DATA = {
         membersName: 'Members',
         onlineCount: 303,
         onlineName: 'Online',
+        isMember: false,
+    },
+    'MBA': {
+        subredditId: 't5_2rbd9',
+        display_name_prefixed: 'r/MBA',
+        // banner_background_image: `https://styles.redditmedia.com/t5_2sokd/styles/bannerBackgroundImag` +
+        // `e_at8s941lsun31.png`,
+        user_is_subscriber: true,
+        public_description_html: `&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;p&gt;Learn about MBA programs, 
+        applying to them, and what life is like while in one and afterwards. Please make sure to read our rules
+         and wiki before posting. &lt;/p&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;`,
+        public_description: `Learn about MBA programs, applying to them, and what life is like while in one and
+         afterwards. Please make sure to read our rules and wiki before posting.`,
+        membersCount: 228000,
+        membersName: 'Members',
+        onlineCount: 131,
+        onlineName: 'Online',
+        isMember: true,
+    },
+    'savedPosts': [
+        {
+            poll: {
+                options: [],
+                votingLength: 0,
+                voters: [],
+            },
+            _id: '6608765a61b91e4253ba96bc',
+            user: '65f5f5c26c3fec25017c306d',
+            title: 'bgrab post connected l swar',
+            text: 'post b swra please',
+            approved: false,
+            approvedBy: '65f5f5c26c3fec25017c306d',
+            disapproved: false,
+            disapprovedBy: '65f5f5c26c3fec25017c306d',
+            url: 'https://example.com',
+            type: 'image',
+            isNSFW: false,
+            isSpoiler: false,
+            isLocked: false,
+            isOc: true,
+            upvotes: 1,
+            downvotes: 0,
+            views: 0,
+            commentCount: 0,
+            __v: 1,
+            createdAt: '2024-04-13T18:24:40.420Z',
+            subredditName: 'csMajors',
+            isUpvoted: false,
+            isDownvoted: false,
+            imageUrls: [],
+        },
+        {
+            poll: {
+                options: [],
+                votingLength: 0,
+                voters: [],
+                startTime: null,
+                endTime: null,
+            },
+            _id: '661ac8c5ba393b109950d416',
+            user: '661910577aceb521f1f65a27',
+            createdAt: '2024-04-13T18:02:45.014Z',
+            subReddit: '661ac6d5ba393b109950d3ed',
+            title: 'amr is here ya fahoud',
+            text: 'lolololollololololololoololo',
+            approved: false,
+            disapproved: false,
+            url: '',
+            type: 'text',
+            isNSFW: true,
+            isSpoiler: false,
+            isLocked: false,
+            upvotes: 1,
+            downvotes: 0,
+            views: 0,
+            commentCount: 0,
+            __v: 0,
+            subredditName: 'Tohotmous brime',
+            isUpvoted: false,
+            isDownvoted: false,
+        },
+        {
+            poll: {
+                options: [],
+                votingLength: 0,
+                voters: [],
+                startTime: null,
+                endTime: null,
+            },
+            _id: '661ac91fba393b109950d41d',
+            user: '661910577aceb521f1f65a27',
+            createdAt: '2024-04-13T18:04:15.204Z',
+            subReddit: '661ac6d5ba393b109950d3ed',
+            title: 'amr is here ya fahoud taniii',
+            text: 'image 34n 5atrak',
+            approved: false,
+            disapproved: false,
+            url: '',
+            type: 'image',
+            isNSFW: false,
+            isSpoiler: true,
+            isLocked: false,
+            upvotes: 1,
+            downvotes: 0,
+            views: 0,
+            commentCount: 0,
+            __v: 0,
+            subredditName: 'Tohotmous brime',
+            isUpvoted: false,
+            isDownvoted: false,
+            imageUrls: [
+                'https://res.cloudinary.com/dfb8f6xbh/image/upload/v1713031456/xudliscxm22bnvxftndh.jpg',
+            ],
+        },
+        {
+            poll: {
+                options: [],
+                votingLength: 0,
+                voters: [],
+                startTime: null,
+                endTime: null,
+            },
+            _id: '661aca1bba393b109950d429',
+            user: '6619d16849a19d248e4b9f43',
+            createdAt: '2024-04-13T18:08:27.427Z',
+            subReddit: '661ac784ba393b109950d3f3',
+            title: 'gaser is here',
+            text: 'image 34n 5ater fahd bas amr m4 radyyy',
+            approved: false,
+            disapproved: false,
+            url: '',
+            type: 'text',
+            isNSFW: false,
+            isSpoiler: false,
+            isLocked: false,
+            upvotes: 1,
+            downvotes: 0,
+            views: 0,
+            commentCount: 0,
+            __v: 0,
+            subredditName: 'Amour elamour',
+            isUpvoted: false,
+            isDownvoted: false,
+        },
+    ],
+    '65f5f5c26c3fec25017c306d': {
+        userId: '65f5f5c26c3fec25017c306d',
+        displayName: 'Marly',
+        publicDescription: 'wana wayaya ba3ee44',
+        commentKarma: 0,
+        created: 1712938904,
+        postKarma: 1,
+        isFriend: false,
+        isBlocked: false,
+        avatar: 'https://res.cloudinary.com/dfb8f6xbh/image/upload/v1713012454/iwdaqrwkrvftxibneye1.png',
+    },
+    '661910577aceb521f1f65a27': {
+        userId: '661910577aceb521f1f65a27',
+        displayName: 'amrashraf',
+        publicDescription: null,
+        commentKarma: 0,
+        created: 1712918615,
+        postKarma: 0,
+        isFriend: false,
+        isBlocked: false,
+        avatar: null,
+    },
+    '6619d16849a19d248e4b9f43': {
+        userId: '6619d16849a19d248e4b9f43',
+        displayName: 'gaser',
+        publicDescription: null,
+        commentKarma: 0,
+        created: 1712968040,
+        postKarma: 0,
+        isFriend: false,
+        isBlocked: false,
+        avatar: null,
+    },
+    'Amour elamour': {
+        name: 'Amour elamour',
+        subredditId: '661ac784ba393b109950d3f3',
+        avatarImage: null,
+        bannerImage: null,
+        description: '',
+        membersNickname: 'Members',
+        membersCount: 2,
+        currentlyViewingNickname: 'Online',
+        currentlyViewingCount: 1,
+        isMember: false,
+    },
+    'Tohotmous brime': {
+        name: 'Tohotmous brime',
+        subredditId: '661ac6d5ba393b109950d3ed',
+        avatarImage: 'https://res.cloudinary.com/dfb8f6xbh/image/upload/v1713038317/nqeilr49ckgm6rvzv9k2.png',
+        bannerImage: 'https://res.cloudinary.com/dfb8f6xbh/image/upload/v1713038368/jggqcbwdgbav1ho7wccv.gif',
+        description: `The Autobots are a fictional faction of sentient robots in the Transformers
+        multimedia franchise.`,
+        membersNickname: 'Autobots',
+        membersCount: 2,
+        currentlyViewingNickname: 'Assemble',
+        currentlyViewingCount: 1,
+        isMember: true,
     },
 };
 
