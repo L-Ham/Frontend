2afd9aa6910fa4d2282e46f4f4055c648da21ec1
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-04-14 14:07:05 +0200

    [Integration] Vote + PostContent

--- src/generic components/Post/PostContent/postcontent.hooks.js
+++ src/generic components/Post/PostContent/postcontent.hooks.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import {PostText} from './PostText/posttext.js';
 import {PostImage} from './PostImage/postimage.js';
-import {DATA} from '../data.js';
 
-export const usePostContent = ({postId, viewContext}) => {
-    const {postType} = DATA[postId];
+export const usePostContent = ({postId, type, text, imageUrls, viewContext}) => {
     let postContent = null;
-    if (postType === 'text') {
-        postContent = <PostText postId={postId} viewContext={viewContext} />;
-    } else if (postType === 'image') {
-        postContent = <PostImage postId={postId} viewContext={viewContext} />;
+    if (type === 'text') {
+        postContent = <PostText postId={postId} content={text} viewContext={viewContext} />;
+    } else if (type === 'image' && imageUrls.length === 1) {
+        postContent = <PostImage postId={postId} image={imageUrls[0]} viewContext={viewContext} />;
     }
     return {
         postContent,
