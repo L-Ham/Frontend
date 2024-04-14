1baf37f21de5fb7086b28726992c7e0ce9446204
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-04-14 14:06:41 +0200

    [Integration] Options + PostInfo

--- src/generic components/Post/PostInfo/ButtonsPanel/OptionsButton/options.styles.js
+++ src/generic components/Post/PostInfo/ButtonsPanel/OptionsButton/options.styles.js
@@ -1,4 +1,4 @@
 export const optionsClasses = {
     root: `z-[1] rounded-[99px] p-2 text-[var(--color-neutral-content-strong)]
-    hover:bg-[var(--color-secondary-background-hover)]`,
+    hover:bg-[var(--color-secondary-background-hover)] active:bg-[var(--color-interactive-pressed)] `,
 };
--- src/generic components/Post/PostInfo/postinfo.hooks.js
+++ src/generic components/Post/PostInfo/postinfo.hooks.js
@@ -1,9 +1,11 @@
 import {DATA} from '../data';
 
-export const usePostInfo = ({postId}) => {
-    const {subreddit_id: subredditId} = DATA[postId];
-    const {user_is_subscriber: isSubscriber} = DATA[subredditId];
+export const usePostInfo = ({subredditName}) => {
+    // request
+    const {isMember, avatar, subredditId} = DATA[subredditName];
     return {
-        isSubscriber,
+        isMember,
+        subredditAvatar: avatar,
+        subredditId,
     };
 };
