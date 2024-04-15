9f4d6fd3ed333ed13f7425fbe8a7875a158dc5eb
mohzakaria <95968167+mohzakaria@users.noreply.github.com>
2024-04-16 01:42:52 +0200

    Hot fix: Created At

--- src/pages/Profile/SideBar/SideBody/PostInfo/postinfo.hook.js
+++ src/pages/Profile/SideBar/SideBody/PostInfo/postinfo.hook.js
@@ -1,12 +1,16 @@
 import {useSelector} from 'react-redux';
 export const usePostInfo = () => {
     const user = useSelector((state) => state.user);
-
-
+    const userCreatedDate = new Date((user.created)*1000); // assuming user.created is a valid date string or timestamp
+    const formattedDate = userCreatedDate.toLocaleDateString('en-GB', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    });
     const info=[
         {info: user.postKarma, title: 'Post Karma'},
         {info: user.commentKarma, title: 'Comment Karma'},
-        {info: `24 April 2024`, title: 'Cake Day'},
+        {info: formattedDate, title: 'Cake Day'},
         {info: '0', title: 'Coins'},
     ];
     return {
