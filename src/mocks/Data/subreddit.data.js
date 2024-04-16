6bafd539830f18c57f6fc98e4e17a9d6e665be59
gaser sami <gaser.elmasry02@eng-st.cu.edu.eg>
2024-04-16 23:04:00 +0200

    Add subreddit mock

---
+++ src/mocks/Data/subreddit.data.js
@@ -0,0 +1,274 @@
+/* eslint-disable max-len*/
+
+export const COMMUNITIES_DETAILS = {
+    'message': 'Subreddit\'s Community Details Retrieved Successfully',
+    'communityDetails': {
+        'name': 'DragonOath',
+        'subredditId': '661afc3579200d572b16e244',
+        'avatarImage': `https://play-lh.googleusercontent.com/
+            tUFv4SQsOcyjuFgXfDJK0XS-R05NF54JbvFsnxS0nV1lUIW53zn6s9I4i1qvRC0wJJ0`,
+        'bannerImage': 'https://genz.com.vn/wp-content/uploads/2022/10/tlbb2-vng-03.gif',
+        'description': `Dragon Oath, also known as Tian Long Ba Bu in China, is a 
+            free-to-play massively multiplayer 
+            online role-playing game developed and 
+            published by Changyou.com and Sohu and 
+            launched in May 2007. The game's story is based on the novel Demi-Gods and Semi-Devils by Louis Cha.`,
+        'membersNickname': 'Members',
+        'membersCount': 2700000,
+        'currentlyViewingNickname': 'Online',
+        'currentlyViewingCount': 23020,
+        'isMember': true,
+        'isFavorite': false,
+        'isMuted': true,
+        'createdAt': 1713044533,
+    },
+};
+
+
+export const WIDGETS = {
+    'message': 'Subreddit widgets retrieved successfully',
+    'communityDetails': {
+        'name': 'Dragon Oath',
+        'subredditId': '661afc3579200d572b16e244',
+        'avatarImage': 'https://shorturl.at/bF489',
+        'bannerImage': `https://encrypted-tbn0.gstatic.com/
+        images?q=tbn:ANd9GcTxW8KSMlu_gON3b0qq8EzkbWvxI5TOpBXi2w&usqp=CAU`,
+        'description': `Dragon Oath, also known as Tian Long Ba Bu in China, 
+        is a free-to-play massively multiplayer online role-playing 
+        game developed and published by Changyou.com and Sohu and
+         launched in May 2007. The game's story is based on the novel Demi-Gods and Semi-Devils by Louis Cha.`,
+        'membersNickname': 'Members',
+        'membersCount': 2700000,
+        'currentlyViewingNickname': 'Online',
+        'currentlyViewingCount': 23020,
+        'isMember': true,
+        'isFavorite': false,
+        'isMuted': true,
+        'createdAt': 1713044533,
+    },
+    'textWidgets': {
+        '661afc7f79200d571f66e258': {
+            'widgetName': 'Hey Message',
+            'text': `The Minstrels are often depicted as ne'er-do-wells and 
+            charlatans who prefer not to meet challenges head on. 
+            They prefer to see themselves as clever strategists, 
+            relying heavily on traps and devices to confuse their enemies.
+             Their devices normally invoke the aid of magic and the element of fire.`,
+            'textHtml': `<p>The Minstrels are often depicted as 
+            ne'er-do-wells and charlatans who prefer 
+            not to meet challenges head on. They prefer to see themselves as clever strategists, relying heavily on
+             traps and devices to confuse their enemies.
+              Their devices normally invoke the aid of magic and the element of fire.</p>`,
+            'shortName': 'Assasin',
+            '_id': '661afc7f79200d571f66e258',
+        },
+    },
+    'moderators': [
+        {
+            'username': 'zMentalist',
+            'avatarImage': '66192b37c528ee5c26825b7e',
+        }, {
+            'username': 'zDoom',
+            'avatarImage': '66192b37c528ee5c26825b7e',
+        }, {
+            'username': 'zDoom',
+            'avatarImage': '66192b37c528ee5c26825b7e',
+        },
+    ],
+    'orderWidget': [
+        '661afc3579200d571f66e243',
+        '661afc7f79200d571f66e258',
+        '661afc8a79200d571f66e25f',
+    ],
+    '661afc3579200d571f66e243': {
+        '_id': '661afc3579200d571f66e243',
+        'ruleList': [
+            {
+                'ruleText': 'No Account Trading or Sales',
+                'reportReason': 'Unauthorized Transactions',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': `- **Do not engage in buying, selling, or trading accounts.**\n- **Report
+                 any solicitations or offers related to account trading.**`,
+                'descriptionHtml': `&lt;!-- SC_OFF --&gt;&lt;div 
+                class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Do not engage in
+                 buying, selling, or trading 
+                 accounts.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Report 
+                 any solicitations or offers related to 
+                 account trading.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;`,
+                '_id': '661afc6887900d571f66e255',
+            },
+            {
+                'ruleText': 'Respect Player Privacy',
+                'reportReason': 'Privacy Violation',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': `- **Do not share personal information of 
+                players without consent.**\n- **No harassment or doxxing.**`,
+                'descriptionHtml': `&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;
+                \n&lt;li&gt;&lt;strong&gt;Do not share personal information of players 
+                without consent.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;No harassment
+                 or doxxing.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;`,
+                '_id': '661afc6979200d571f66e258',
+            },
+            {
+                'ruleText': 'No Cheat Codes or Exploits',
+                'reportReason': 'Cheating',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': `- **Posting cheats or exploits is forbidden.
+                **\n- **Report any exploits or unfair gameplay advantages.**`,
+                'descriptionHtml': `&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;
+                Posting cheats or exploits is forbidden
+                .&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Report any exploits 
+                or unfair gameplay advantages.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;
+                \n&lt;/div&gt;&lt;!-- SC_ON --&gt;`,
+                '_id': '661afc6a79200d571f66e25b',
+            },
+            {
+                'ruleText': 'Content Posting Guidelines',
+                'reportReason': 'Irrelevant Content',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': `- **All posts must be directly related to 
+                Dragon Oath.**\n- **Avoid reposting frequently shared content.**`,
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;All posts must be directly related to Dragon Oath.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Avoid reposting frequently shared content.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6b79200d571f66e25e',
+            },
+            {
+                'ruleText': 'Language and Behavior',
+                'reportReason': 'Offensive Language',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Use respectful language only.**\n- **No offensive or discriminatory remarks.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Use respectful language only.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;No offensive or discriminatory remarks.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6c79200d571f66e261',
+            },
+            {
+                'ruleText': 'Event Spoilers',
+                'reportReason': 'Improper Spoilers',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Use spoiler tags for new events.**\n- **Do not include spoilers in titles.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Use spoiler tags for new events.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Do not include spoilers in titles.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6d79200d571f66e264',
+            },
+            {
+                'ruleText': 'Fan Art and Media',
+                'reportReason': 'Copyright Infringement',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Credit the original creators of fan art.**\n- **Do not use fan-created content for commercial purposes without permission.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Credit the original creators of fan art.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Do not use fan-created content for commercial purposes without permission.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6e79200d571f66e267',
+            },
+            {
+                'ruleText': 'No Spam or Low-effort Posts',
+                'reportReason': 'Spam',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Avoid posting repetitive or low-effort content.**\n- **Encourage meaningful community interaction.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Avoid posting repetitive or low-effort content.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Encourage meaningful community interaction.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6f79200d571f66e26a',
+            },
+            {
+                'ruleText': 'Reporting In-Game Issues',
+                'reportReason': 'Misuse of Forum',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Use official channels for technical support and bug reports.**\n- **Keep discussion focused on game experiences and community support.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Use official channels for technical support and bug reports.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Keep discussion focused on game experiences and community support.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc7079200d571f66e26d',
+            },
+            {
+                'ruleText': 'Respect the Moderation Team',
+                'reportReason': 'Disrespect to Moderators',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Follow moderator instructions to maintain community standards.**\n- **Handle moderation disputes through private channels.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Follow moderator instructions to maintain community standards.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Handle moderation disputes through private channels.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc7179200d571f66e270',
+            },
+        ],
+    },
+};
+
+
+export const SUBREDDIT_RULES = {
+    'message': 'Subreddit rules retrieved successfully',
+    'rules': {
+        '_id': '661ae6f9b0581bfc80197bc0',
+        'ruleList': [
+            {
+                'ruleText': 'No Account Trading or Sales',
+                'reportReason': 'Unauthorized Transactions',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Do not engage in buying, selling, or trading accounts.**\n- **Report any solicitations or offers related to account trading.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Do not engage in buying, selling, or trading accounts.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Report any solicitations or offers related to account trading.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6887900d571f66e255',
+            },
+            {
+                'ruleText': 'Respect Player Privacy',
+                'reportReason': 'Privacy Violation',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Do not share personal information of players without consent.**\n- **No harassment or doxxing.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Do not share personal information of players without consent.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;No harassment or doxxing.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6979200d571f66e258',
+            },
+            {
+                'ruleText': 'No Cheat Codes or Exploits',
+                'reportReason': 'Cheating',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Posting cheats or exploits is forbidden.**\n- **Report any exploits or unfair gameplay advantages.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Posting cheats or exploits is forbidden.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Report any exploits or unfair gameplay advantages.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6a79200d571f66e25b',
+            },
+            {
+                'ruleText': 'Content Posting Guidelines',
+                'reportReason': 'Irrelevant Content',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **All posts must be directly related to Dragon Oath.**\n- **Avoid reposting frequently shared content.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;All posts must be directly related to Dragon Oath.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Avoid reposting frequently shared content.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6b79200d571f66e25e',
+            },
+            {
+                'ruleText': 'Language and Behavior',
+                'reportReason': 'Offensive Language',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Use respectful language only.**\n- **No offensive or discriminatory remarks.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Use respectful language only.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;No offensive or discriminatory remarks.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6c79200d571f66e261',
+            },
+            {
+                'ruleText': 'Event Spoilers',
+                'reportReason': 'Improper Spoilers',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Use spoiler tags for new events.**\n- **Do not include spoilers in titles.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Use spoiler tags for new events.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Do not include spoilers in titles.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6d79200d571f66e264',
+            },
+            {
+                'ruleText': 'Fan Art and Media',
+                'reportReason': 'Copyright Infringement',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Credit the original creators of fan art.**\n- **Do not use fan-created content for commercial purposes without permission.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Credit the original creators of fan art.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Do not use fan-created content for commercial purposes without permission.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6e79200d571f66e267',
+            },
+            {
+                'ruleText': 'No Spam or Low-effort Posts',
+                'reportReason': 'Spam',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Avoid posting repetitive or low-effort content.**\n- **Encourage meaningful community interaction.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Avoid posting repetitive or low-effort content.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Encourage meaningful community interaction.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc6f79200d571f66e26a',
+            },
+            {
+                'ruleText': 'Reporting In-Game Issues',
+                'reportReason': 'Misuse of Forum',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Use official channels for technical support and bug reports.**\n- **Keep discussion focused on game experiences and community support.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Use official channels for technical support and bug reports.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Keep discussion focused on game experiences and community support.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc7079200d571f66e26d',
+            },
+            {
+                'ruleText': 'Respect the Moderation Team',
+                'reportReason': 'Disrespect to Moderators',
+                'appliesTo': 'Posts and comments',
+                'fullDescription': '- **Follow moderator instructions to maintain community standards.**\n- **Handle moderation disputes through private channels.**',
+                'descriptionHtml': '&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;ul&gt;\n&lt;li&gt;&lt;strong&gt;Follow moderator instructions to maintain community standards.&lt;/strong&gt;&lt;/li&gt;\n&lt;li&gt;&lt;strong&gt;Handle moderation disputes through private channels.&lt;/strong&gt;&lt;/li&gt;\n&lt;/ul&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;',
+                '_id': '661afc7179200d571f66e270',
+            },
+        ],
+    },
+};
