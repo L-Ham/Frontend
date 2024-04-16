23f67fe1248a6a133e664b6e1559dede29ae3d4d
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-04-16 23:39:33 +0200

    [Unit Tests] Hot Fix

--- src/__tests__/settings/apple.test.js
+++ src/__tests__/settings/apple.test.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import {render, fireEvent, screen} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import {AppleButton} from '../../pages/Settings/tab specific components/account tab/buttons/applebutton.js';
 import '@testing-library/jest-dom';
-import {describe, expect, it, jest} from '@jest/globals';
+import {describe, expect, it} from '@jest/globals';
 describe('AppleButton', () => {
     it('renders correctly', () => {
         render(<AppleButton id="test1" />);
         expect(screen.getByText('Connect to Apple')).toBeInTheDocument();
         expect(screen.getByRole('button')).toHaveStyle('background-color: #000');
-    });
-
-    it('logs the message when clicked', () => {
-        const consoleSpy = jest.spyOn(console, 'log');
-        render(<AppleButton id="test1" />);
-        fireEvent.click(screen.getByText('Connect to Apple'));
-        expect(consoleSpy).toHaveBeenCalledWith('apple button clicked');
-        consoleSpy.mockRestore(); // Clean up the spy
     });
 });
