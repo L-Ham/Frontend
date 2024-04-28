f5f99d96fc6e1db40f2b647ac3f063e60a53124b
gaser sami <gaser.elmasry02@eng-st.cu.edu.eg>
2024-04-28 19:51:08 +0300

    Add notifications components and styles

---
+++ src/generic components/Notifications/Notification/notification.hooks.js
@@ -0,0 +1,38 @@
+import {useEffect} from 'react';
+import {getIconComponent} from '../../iconsmap';
+
+export const useNotification = ({notification, onClose}) => {
+    if (!notification) return null;
+
+    const {type, id} = notification;
+    const HappyFaceIcon = getIconComponent('happy-face');
+    const SadFaceIcon = getIconComponent('sad-face');
+    const Icon = type === 'success' ? HappyFaceIcon : SadFaceIcon;
+    const CloseIcon = getIconComponent('close');
+
+    const autoClose = () => {
+        // add class scale-down and remove class scale-up
+        const toaster = document.getElementById('toaster');
+        if (!toaster) {
+            console.error('Element with id "toaster" not found');
+            return;
+        }
+        toaster.classList.remove('scale-up');
+        toaster.classList.add('scale-down');
+        setTimeout(() => {
+            onClose(id);
+        }, 300);
+    };
+
+    const handleClose = () => {
+        onClose(id);
+    };
+
+    useEffect(() => {
+        if (type == 'success') {
+            setTimeout(autoClose, 5000);
+        }
+    }, []);
+
+    return {Icon, CloseIcon, handleClose};
+};
