c50a683806334e2589d0194ab4c83a234adc8832
gaser sami <gaser.elmasry02@eng-st.cu.edu.eg>
2024-05-02 16:14:25 +0300

    add and update and fix Notifications

--- src/layouts/Header/RightItems/Buttons/NotificationButton/notificationsbutton.js
+++ src/layouts/Header/RightItems/Buttons/NotificationButton/notificationsbutton.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {getIconComponent} from '../../../../../generic components/iconsmap.js';
 import {NotificationsContainer} from './Notifications/notificationscontainer.js';
 import {actionButtonClasses as styles} from '../buttons.styles.js';
+import {useNotificationsButtons} from './notificationsbuttoncontext.js';
 import '../../../appbar.css';
+import './notificationsbutton.css';
 
 /**
  * @component
@@ -11,6 +13,7 @@ import '../../../appbar.css';
 function NotificationButton() {
     const Icon = getIconComponent('low', false);
     const [isVisible, setIsVisible] = React.useState(false);
+    const {unreadNotifications} = useNotificationsButtons();
 
     const handleClick = () => {
         setIsVisible(!isVisible);
@@ -18,8 +21,13 @@ function NotificationButton() {
 
     return (
         <div className='relative m-0 p-0' datatestid="notif-button-outer-continer-#TDSF&YOISNADJA(D*">
-            <button className={styles.root} onClick={handleClick} data-testid={`action-button-low-##33443@@-geso`}>
+            <button className={`${styles.root} relative top-1`}
+                onClick={handleClick} data-testid={`action-button-low-##33443@@-geso`}>
                 <span className={styles.iconContainer} data-testid={`action-button-icon-container-low-#534#@_90-gesoo`}>
+                    {unreadNotifications != 0 &&
+                   <span className="_1-nIsCaWhGBFN-L4ZHnbGp">
+                       {unreadNotifications}
+                   </span>}
                     <Icon />
                 </span>
             </button>
