ae2504929a1f58f51982272dad1675607e78d805
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-05-10 00:10:41 +0300

    Comment Content type

---
+++ src/pages/CommentsPage/Comments/CommentTree/Comment/Reply/commentreply.styles.js
@@ -0,0 +1,11 @@
+export const commentsClasses = {
+    wrapper: `flex shrink items-center`,
+    icon: `mr-1 flex text-base leading-5`,
+};
+
+export const commentsStyles = {
+    root: {
+        height: '2rem',
+        font: 'var(--font-12-16-semibold)',
+    },
+};
